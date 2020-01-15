4998483fbd281b35db64d82d75c5e2b99de70c34
Mikita Aleksandrovich <45469739+MikitaAleksandrovich@users.noreply.github.com>
2020-01-15 21:59:03 +0300

    Work with data binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
 
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer = false;
+  serverCreationStatus = 'No Server was created!';
+  serverName = '';
+
+  userName = '';
+  allowResetUserName = false;
+
+  constructor() { 
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+    
+  }
 
   ngOnInit() {
-    
+    console.log(this.allowNewServer);
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = `Server was created! Name is ${this.serverName}`;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  resetUserName() {
+    this.userName = '';
   }
 
 }
